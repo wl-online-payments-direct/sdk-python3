057d5fc38d6c299339a18e9335e0c97bbb668b2b
Worldline Direct support team <82139942+worldline-direct-support-team@users.noreply.github.com>
2023-06-19 12:30:49 +0200

    Release version 4.11.0.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def test_collector():
 
 setup(
     name="onlinepayments-sdk-python3",
-    version="4.10.0",
+    version="4.11.0",
     author="Worldline Direct support team",
     author_email="82139942+worldline-direct-support-team@users.noreply.github.com",
     description="SDK to communicate with the Online Payments platform using the Online Payments  Server API",
